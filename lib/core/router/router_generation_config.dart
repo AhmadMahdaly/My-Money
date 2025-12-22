6bcd35bae1b94bb9706ae2e5c4c0c6457ab71a28
Ahmed Mahmoud <ahmed.mahdaly86@gmail.com>
2025-12-22 10:23:10 +0200

    improve

--- lib/core/router/router_generation_config.dart
+++ lib/core/router/router_generation_config.dart
@@ -3,6 +3,7 @@ import 'package:opration/core/router/app_routes.dart';
 import 'package:opration/features/financial_goals/presentation/screens/financial_goals_screen.dart';
 import 'package:opration/features/intro/login/presentation/views/login_view.dart';
 import 'package:opration/features/intro/splash/views/splash_view.dart';
+import 'package:opration/features/main_layout/views/main_layout.dart';
 import 'package:opration/features/transactions/domain/entities/transaction.dart';
 import 'package:opration/features/transactions/presentation/screens/add_transaction_screen.dart';
 import 'package:opration/features/transactions/presentation/screens/edit_transaction_screen.dart';
@@ -25,11 +26,11 @@ class RouterGenerationConfig {
         name: AppRoutes.loginScreen,
         builder: (context, state) => const LoginScreen(),
       ),
-      // GoRoute(
-      //   path: AppRoutes.mainLayout,
-      //   name: AppRoutes.mainLayout,
-      //   builder: (context, state) => const MainLayout(),
-      // ),
+      GoRoute(
+        path: AppRoutes.mainLayout,
+        name: AppRoutes.mainLayout,
+        builder: (context, state) => const MainLayout(),
+      ),
       GoRoute(
         path: AppRoutes.addTransactionScreen,
         name: AppRoutes.addTransactionScreen,
--- lib/features/main_layout/cubit/main_layout_cubit.dart
+++ lib/features/main_layout/cubit/main_layout_cubit.dart
@@ -1,25 +1,25 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:opration/features/financial_goals/presentation/screens/financial_goals_screen.dart';
-// import 'package:opration/features/transactions/presentation/screens/add_transaction_screen.dart';
-// import 'package:opration/features/transactions/presentation/screens/monthly_plan_screen.dart';
-// import 'package:opration/features/transactions/presentation/screens/transaction_details_screen.dart';
-// import 'package:opration/features/wallets/presentation/screens/wallets_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:opration/features/financial_goals/presentation/screens/financial_goals_screen.dart';
+import 'package:opration/features/transactions/presentation/screens/add_transaction_screen.dart';
+import 'package:opration/features/transactions/presentation/screens/monthly_plan_screen.dart';
+import 'package:opration/features/transactions/presentation/screens/transaction_details_screen.dart';
+import 'package:opration/features/wallets/presentation/screens/wallets_screen.dart';
 
-// part 'main_layout_state.dart';
+part 'main_layout_state.dart';
 
-// class MainLayoutCubit extends Cubit<MainLayoutState> {
-//   MainLayoutCubit() : super(MainLayoutInitial());
-//   List<Widget> screens = [
-//     const TransactionDetailsScreen(),
-//     const WalletsScreen(),
-//     const AddTransactionScreen(),
-//     const MonthlyPlanScreen(),
-//     const FinancialGoalsScreen(),
-//   ];
-//   int currentIndex = 2;
-//   void changeNavBarIndex(int index) {
-//     currentIndex = index;
-//     emit(ChangeNavBarState());
-//   }
-// }
+class MainLayoutCubit extends Cubit<MainLayoutState> {
+  MainLayoutCubit() : super(MainLayoutInitial());
+  List<Widget> screens = [
+    const TransactionDetailsScreen(),
+    const WalletsScreen(),
+    const AddTransactionScreen(),
+    const MonthlyPlanScreen(),
+    const FinancialGoalsScreen(),
+  ];
+  int currentIndex = 2;
+  void changeNavBarIndex(int index) {
+    currentIndex = index;
+    emit(ChangeNavBarState());
+  }
+}
